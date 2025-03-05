28ce87a5063278668983dc7be03c16b54935dad8
Sarfraz-Ahmad-it <sarfrazahmadiit@gmail.com>
2025-03-05 17:27:48 +0530

    add code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let h = document.getElementById('h1');
+let inputs = document.getElementById('inputt');
+let text = document.querySelector('.text');
+let button = document.querySelector('button');
+let un = document.querySelector('ul');
+
+button.addEventListener('click', ()=>{
+   if(inputs.value == ""){
+    alert("please enter value")
+   }
+   else{
+    text.innerHTML = inputs.value;
+   }
+})
